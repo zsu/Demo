4b164843b1ec4e3eb6de87e0448e6bdf79eca401
zsu <azure.ss@gmail.com>
2017-01-07 20:50:39 -0500

    v1

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,19 @@
+﻿using SessionMessage;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo
+{
+	public class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+			filters.Add(new MvcAjaxMessagesFilterAttribute());
+		}
+		public static void RegisterWebApiGlobalFilters()
+		{
+			System.Web.Http.GlobalConfiguration.Configuration.Filters.Add(new AjaxMessagesFilterAttribute());
+		}
+	}
+}
